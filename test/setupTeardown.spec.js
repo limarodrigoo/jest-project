f745914954d90d175947e99a8eccaac3a3f72fe1
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-23 19:41:39 -0300

    refactor requit 04

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,29 +1,23 @@
 const adventure = require('../src/setupTeardown');
 
 describe('4 - Quem sobreviveu?', () => {
-  test('depois da primeira aventura', () => {
+  beforeEach(() => {
     adventure.randomAttack();
     console.log(`Grupo restante ${adventure.specialists.map((specialist) => specialist.nome)}`);
+  });
+  test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
-    adventure.randomAttack();
-    console.log(`Grupo restante ${adventure.specialists.map((specialist) => specialist.nome)}`);
     expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
-    adventure.randomAttack();
-    console.log(`Grupo restante ${adventure.specialists.map((specialist) => specialist.nome)}`);
     expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
-    adventure.randomAttack();
-    console.log(`Grupo restante ${adventure.specialists.map((specialist) => specialist.nome)}`);
     expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
-    adventure.randomAttack();
-    console.log(`Sobrevivente ${adventure.specialists.map((specialist) => specialist.nome)}`);
     expect(adventure.specialists.length).toBe(1);
   });
 });
